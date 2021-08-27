f1c852278ca7573ad0087185de54b699c4b3f054
israaghanem <49322174+israaghanem@users.noreply.github.com>
2021-08-27 14:29:57 +0200

    Update ReversedOrder.c

--- ReversedOrder.c
+++ ReversedOrder.c
@@ -2,16 +2,21 @@
 
 int main(void)
 {
+	//Identify Variables I will reverse
 	int FirstNum , ScondNum , ThirdNum ;
 	
+	// Get the first variable
 	printf("\nPlease Enter First Number : ");
 	scanf("%d",&FirstNum);
 	
+	// Get the second variable
 	printf("Please Enter Second Number : ");
 	scanf("%d",&ScondNum);
 	
+	// Get the third variable
 	printf("Please Enter Third Number : ");
 	scanf("%d",&ThirdNum);	
 	
+	//print then in a reversed order 
 	printf("Third Number = %d \nSecond Number = %d\nFirst Number = %d\n",ThirdNum , ScondNum , FirstNum);
 }
